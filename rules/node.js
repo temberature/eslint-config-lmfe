7de90694ee326742199d62fa0bb94f8468b683ec
孟辰 <mengchen@corp.netease.com>
2018-02-06 18:33:13 +0800

    Update node.js

--- rules/node.js
+++ rules/node.js
@@ -9,24 +9,24 @@ module.exports = {
 
     // require all requires be top-level
     // https://eslint.org/docs/rules/global-require
-    "global-require": "error",
+    "global-require": "off",
 
     // enforces error handling in callbacks (node environment)
     "handle-callback-err": "off",
 
     // disallow use of the Buffer() constructor
     // https://eslint.org/docs/rules/no-buffer-constructor
-    "no-buffer-constructor": "error",
+    "no-buffer-constructor": "warn",
 
     // disallow mixing regular variable and require declarations
     "no-mixed-requires": ["off", false],
 
     // disallow use of new operator with the require function
-    "no-new-require": "error",
+    "no-new-require": "warn",
 
     // disallow string concatenation with __dirname and __filename
     // https://eslint.org/docs/rules/no-path-concat
-    "no-path-concat": "error",
+    "no-path-concat": "warn",
 
     // disallow use of process.env
     "no-process-env": "off",
