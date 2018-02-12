5dc730533841960435d786c1dc835e2ba58993bb
孟辰 <mengchen@corp.netease.com>
2018-02-12 11:41:02 +0800

    完善es6规则

--- rules/es6.js
+++ rules/es6.js
@@ -10,91 +10,40 @@ module.exports = {
       objectLiteralDuplicateProperties: false
     }
   },
-
   rules: {
-    // enforces no braces where they can be omitted
-    // https://eslint.org/docs/rules/arrow-body-style
-    // TODO: enable requireReturnForObjectLiteral?
+    // https://eslint.org/docs/rules/#ecmascript-6
     "arrow-body-style": [
-      "warn",
+      "error",
       "as-needed",
       {
         requireReturnForObjectLiteral: false
       }
-    ],
-
-    // require parens in arrow function arguments
-    // https://eslint.org/docs/rules/arrow-parens
+    ], // 在箭头函数体中需要大括号
     "arrow-parens": [
-      "warn",
+      "error",
       "as-needed",
       {
         requireForBlockBody: true
       }
-    ],
-
-    // require space before/after arrow function's arrow
-    // https://eslint.org/docs/rules/arrow-spacing
-    "arrow-spacing": ["warn", { before: true, after: true }],
-
-    // verify super() callings in constructors
-    "constructor-super": "warn",
-
-    // enforce the spacing around the * in generator functions
-    // https://eslint.org/docs/rules/generator-star-spacing
-    "generator-star-spacing": ["warn", { before: false, after: true }],
-
-    // disallow modifying variables of class declarations
-    // https://eslint.org/docs/rules/no-class-assign
-    "no-class-assign": "warn",
-
-    // disallow arrow functions where they could be confused with comparisons
-    // https://eslint.org/docs/rules/no-confusing-arrow
+    ], // 在箭头函数参数中需要括号
+    "arrow-spacing": ["error", { before: true, after: true }], // 在箭头函数的箭头之前/之后需要空格
+    "constructor-super": "error", // 在constructor中验证super()的调用
+    "generator-star-spacing": ["error", { before: false, after: true }], // 强制在generator的*周围加上空格
+    "no-class-assign": "warn", // 不允许修改已声明的class
     "no-confusing-arrow": [
       "warn",
       {
         allowParens: true
       }
-    ],
-    "no-const-assign": "warn", // 禁止给const声明的变量重新赋值
-
-    // disallow duplicate class members
-    // https://eslint.org/docs/rules/no-dupe-class-members
-    "no-dupe-class-members": "warn",
-
-    // disallow importing from the same path more than once
-    // https://eslint.org/docs/rules/no-duplicate-imports
-    // replaced by https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-    "no-duplicate-imports": "warn",
-
-    // disallow symbol constructor
-    // https://eslint.org/docs/rules/no-new-symbol
-    "no-new-symbol": "warn",
-
-    // disallow specific imports
-    // https://eslint.org/docs/rules/no-restricted-imports
-    "no-restricted-imports": [
-      "off",
-      {
-        paths: [],
-        patterns: []
-      }
-    ],
-
-    // disallow to use this/super before super() calling in constructors.
-    // https://eslint.org/docs/rules/no-this-before-super
-    "no-this-before-super": "warn",
-
-    // disallow useless computed property keys
-    // https://eslint.org/docs/rules/no-useless-computed-key
-    "no-useless-computed-key": "warn",
-
-    // disallow unnecessary constructor
-    // https://eslint.org/docs/rules/no-useless-constructor
-    "no-useless-constructor": "warn",
-
-    // disallow renaming import, export, and destructured assignments to the same name
-    // https://eslint.org/docs/rules/no-useless-rename
+    ], // 混淆比较的箭头
+    "no-const-assign": "warn", // 禁止给const变量赋值
+    "no-dupe-class-members": "warn", // 禁止重复的类成员
+    "no-duplicate-imports": "warn", // 禁止重复导入
+    "no-new-symbol": "error", // 不允new一个symbol
+    "no-restricted-imports": "off", // 禁止导入的模块名称
+    "no-this-before-super": "error", // 在构造函数中调用supe之前不允许出现this/super
+    "no-useless-computed-key": "warn", // 禁止在对象文字中使用不必要的计算属性键
+    "no-useless-constructor": "warn", // 禁止不必要的constructor
     "no-useless-rename": [
       "warn",
       {
@@ -102,35 +51,23 @@ module.exports = {
         ignoreImport: false,
         ignoreExport: false
       }
-    ],
-
-    // require let or const instead of var
-    "no-var": "warn",
-
-    // require method and property shorthand syntax for object literals
-    // https://eslint.org/docs/rules/object-shorthand
-    "object-shorthand": "off",
-
-    // suggest using arrow functions as callbacks
+    ], // 禁止将导入，导出和解构分配重命名为相同的名称
+    "no-var": "warn", // 禁用var
+    "object-shorthand": "off", // 对象语法简写
     "prefer-arrow-callback": [
       "warn",
       {
         allowNamedFunctions: false,
         allowUnboundThis: true
       }
-    ],
-
-    // suggest using of const declaration for variables that are never modified after declared
+    ], // 优先使用箭头函数进行回调
     "prefer-const": [
       "warn",
       {
         destructuring: "any",
         ignoreReadBeforeAssign: true
       }
-    ],
-
-    // Prefer destructuring from arrays and objects
-    // https://eslint.org/docs/rules/prefer-destructuring
+    ], // 优先对声明后不改变的变量使用const
     "prefer-destructuring": [
       "warn",
       {
@@ -146,57 +83,16 @@ module.exports = {
       {
         enforceForRenamedProperties: false
       }
-    ],
-
-    // disallow parseInt() in favor of binary, octal, and hexadecimal literals
-    // https://eslint.org/docs/rules/prefer-numeric-literals
-    "prefer-numeric-literals": "warn",
-
-    // suggest using Reflect methods where applicable
-    // https://eslint.org/docs/rules/prefer-reflect
-    "prefer-reflect": "off",
-
-    // use rest parameters instead of arguments
-    // https://eslint.org/docs/rules/prefer-rest-params
-    "prefer-rest-params": "warn",
-
-    // suggest using the spread operator instead of .apply()
-    // https://eslint.org/docs/rules/prefer-spread
-    "prefer-spread": "warn",
-
-    // suggest using template literals instead of string concatenation
-    // https://eslint.org/docs/rules/prefer-template
-    "prefer-template": "warn",
-
-    // disallow generator functions that do not have yield
-    // https://eslint.org/docs/rules/require-yield
-    "require-yield": "warn",
-
-    // enforce spacing between object rest-spread
-    // https://eslint.org/docs/rules/rest-spread-spacing
-    "rest-spread-spacing": ["warn", "never"],
-
-    // import sorting
-    // https://eslint.org/docs/rules/sort-imports
-    "sort-imports": [
-      "off",
-      {
-        ignoreCase: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["none", "all", "multiple", "single"]
-      }
-    ],
-
-    // require a Symbol description
-    // https://eslint.org/docs/rules/symbol-description
-    "symbol-description": "warn",
-
-    // enforce usage of spacing in template strings
-    // https://eslint.org/docs/rules/template-curly-spacing
-    "template-curly-spacing": "warn",
-
-    // enforce spacing around the * in yield* expressions
-    // https://eslint.org/docs/rules/yield-star-spacing
-    "yield-star-spacing": ["warn", "after"]
+    ], // 优先解构声明
+    "prefer-numeric-literals": "warn", // 不允许parseInt/Number.parseInt转换二进制，八进制和十六进制
+    "prefer-rest-params": "warn", // 建议使用参数而不是arguments
+    "prefer-spread": "warn", // 建议使用扩展运算符
+    "prefer-template": "warn", // 建议使用模板字符串
+    "require-yield": "warn", // 禁止没有yield的generator
+    "rest-spread-spacing": ["warn", "never"], // 扩展运算符的空格
+    "sort-imports": "off", // 导入排序
+    "symbol-description": "warn", // symbol描述
+    "template-curly-spacing": "warn", // 在模板字符串中强制使用空格
+    "yield-star-spacing": ["error", "after"] // 在yield *表达式的*周围加上间距
   }
 };
